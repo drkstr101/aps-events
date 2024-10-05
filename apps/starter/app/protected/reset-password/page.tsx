3d7482e0107e6ee65caa3cac6da20c1fe0f105fd
Aaron Miller <amiller@aps.org>
2024-10-05 03:10:14 +0000

    feat: add supabase starter

---
+++ apps/starter/app/protected/reset-password/page.tsx
@@ -0,0 +1,20 @@
+import { FormMessage, Message } from '../../../components/form-message';
+import { SubmitButton } from '../../../components/submit-button';
+import { Input } from '../../../components/ui/input';
+import { Label } from '../../../components/ui/label';
+import { resetPasswordAction } from '../../actions';
+
+export default async function ResetPassword({ searchParams }: { searchParams: Message }) {
+  return (
+    <form className="flex w-full max-w-md flex-col gap-2 p-4 [&>input]:mb-4">
+      <h1 className="text-2xl font-medium">Reset password</h1>
+      <p className="text-foreground/60 text-sm">Please enter your new password below.</p>
+      <Label htmlFor="password">New password</Label>
+      <Input type="password" name="password" placeholder="New password" required />
+      <Label htmlFor="confirmPassword">Confirm password</Label>
+      <Input type="password" name="confirmPassword" placeholder="Confirm password" required />
+      <SubmitButton formAction={resetPasswordAction}>Reset password</SubmitButton>
+      <FormMessage message={searchParams} />
+    </form>
+  );
+}
